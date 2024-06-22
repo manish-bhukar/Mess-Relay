cc1e33abeb7aaddb2bb6fca9d38cab0c758ce5c8
Manish bhukar <125037273+manish-bhukar@users.noreply.github.com>
2024-06-22 22:20:10 +0530

    Minor changes

--- Backend/index.js
+++ Backend/index.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(fileUpload());
-
+app.use("/uploads",express.static("uploads"));
 
 app.use('/user', userRoutes);
 app.use("/complaints", complaintsRoutes);
