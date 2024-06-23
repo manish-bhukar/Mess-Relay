293d5c6fe5be6e74536676eac7f56fa2ec4f9cd9
Manish bhukar <125037273+manish-bhukar@users.noreply.github.com>
2024-06-23 22:26:56 +0530

    Protected route frontend added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,28 +1,44 @@
-import './App.css';
-import {BrowserRouter as Router,Routes,Route} from "react-router-dom";
-import Signup from './Component/Signup';
-import Login from './Component/Login';
-import StudentDashboard from '../src/Component//Student/Frontpage'
-import StudentComplaint from '../src/Component/Student/Complaint'
-import ContactPage from './Component/Student/ContactPage';
-import AccountantDashboard from '../src/Component/Accountant/Dashboard'
-import ComplainStatus from '../src/Component/Student/Complainstatus'
-import ChiefWardenDashboard from '../src/Component/ChiefWarden/Dashboard.js';
-import AddNotice from './Component/ChiefWarden/Notice.js';
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Signup from "./Component/Signup";
+import Login from "./Component/Login";
+import StudentDashboard from "../src/Component//Student/Frontpage";
+import StudentComplaint from "../src/Component/Student/Complaint";
+import ContactPage from "./Component/Student/ContactPage";
+import AccountantDashboard from "../src/Component/Accountant/Dashboard";
+import ComplainStatus from "../src/Component/Student/Complainstatus";
+import ChiefWardenDashboard from "../src/Component/ChiefWarden/Dashboard.js";
+import AddNotice from "./Component/ChiefWarden/Notice.js";
+import ProtectedRoute from "./Component/ProtectedRoute.js";
 function App() {
   return (
     <div className="App">
       <Router>
         <Routes>
           <Route path="/" element={<Signup />} />
-          <Route path="/login" element={<Login/>}/>
-          <Route path="/studentmain" element={<StudentDashboard />}/>
-          <Route path="/complain" element={<StudentComplaint/>}/>
-          <Route path='/contact' element={<ContactPage/>}/>
-          <Route path="/accountant" element={<AccountantDashboard/>}/>
-          <Route path="/complainstatus" element={<ComplainStatus/>}/>
-          <Route path="/chiefwarden" element={<ChiefWardenDashboard/>}/>
-          <Route path="/notice" element={<AddNotice/>}/>
+          <Route path="/login" element={<Login />} />
+          <Route path="/studentmain" element={<StudentDashboard />} />
+          <Route path="/complain" element={<StudentComplaint />} />
+          <Route path="/contact" element={<ContactPage />} />
+          <Route
+            path="/accountant"
+            element={
+              <ProtectedRoute>
+                <AccountantDashboard />
+              </ProtectedRoute>
+            }
+          />
+          <Route path="/complainstatus" element={
+            <ProtectedRoute>
+              <ComplainStatus />
+            </ProtectedRoute>
+          }
+             />
+          <Route path="/chiefwarden" element={<ProtectedRoute>
+            <ChiefWardenDashboard />
+            </ProtectedRoute>} 
+            />
+          <Route path="/notice" element={<AddNotice />} />
         </Routes>
       </Router>
     </div>
