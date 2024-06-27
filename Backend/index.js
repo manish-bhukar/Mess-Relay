3cdfa10878e5e9fa5dcf6e9fb65d7f2ef2184748
Manish bhukar <125037273+manish-bhukar@users.noreply.github.com>
2024-06-27 08:18:54 +0530

    Deploy changes

--- Backend/index.js
+++ Backend/index.js
@@ -26,14 +26,17 @@ app.use("/messmenu",messMenuRoutes)
 app.use('/user', userRoutes);
 app.use("/complaints", complaintsRoutes);
 app.use('/expenses', expenseRoutes);
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}).then(() => {
-  console.log('Connected to MongoDB');
+mongoose
+  .connect(
+      process.env.MONGO_URI,
+    {useNewUrlParser: true, useUnifiedTopology: true}
+  )
+  .then((data) => {
+    console.log(`Database connected with server:${data.connection.host}`);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
   app.listen(process.env.PORT || 5000, () => {
     console.log(`Server is running on port ${process.env.PORT || 5000}`);
   });
-}).catch((error) => {
-  console.error('Error connecting to MongoDB:', error);
-});
